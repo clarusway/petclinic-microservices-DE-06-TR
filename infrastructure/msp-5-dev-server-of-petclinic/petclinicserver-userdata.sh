fe9bfd2636bf34c4d254d5529e1bd3a9bc86b775
AlexDuncan2100 <alexad2100@gmail.com>
2024-01-03 09:35:38 +0000

    docker-compose version is updated

--- infrastructure/msp-5-dev-server-of-petclinic/petclinicserver-userdata.sh
+++ infrastructure/msp-5-dev-server-of-petclinic/petclinicserver-userdata.sh
@@ -5,7 +5,7 @@ dnf install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
-curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 dnf install git -y
 dnf install java-11-amazon-corretto -y
